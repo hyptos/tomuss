9a0d438ec968e0c7e189184f1c8f8f9a6b313f7d
Antoine Martin <hyptos974@gmail.com>
2016-09-19 15:54:33 +0200

    ajout du lint google

--- app/tomuss.js
+++ app/tomuss.js
@@ -6,80 +6,78 @@
  * Licensed under the MIT license.
  */
 
-'use strict';
+let page = require('webpage').create();
 
-var page = require('webpage').create();
-var loadInProgress = false;
-var testindex = 0;
+let loadInProgress = false;
+let testindex = 0;
 
-// Route "console.log()" calls from within the Page context to the main Phantom context (i.e. current "this")
-page.onConsoleMessage = function(msg) {
+page.onConsoleMessage = function onConsoleMessage(msg) {
     console.log(msg);
 };
 
-page.onAlert = function(msg) {
+page.onAlert = function onAlert(msg) {
     console.log('alert!!> ' + msg);
 };
 
-page.onLoadStarted = function() {
+page.onLoadStarted = function onLoadStarted() {
     loadInProgress = true;
-    console.log("load started");
+    console.log('load started');
 };
 
-page.onLoadFinished = function(status) {
+page.onLoadFinished = function onLoadFinished(status) {
     loadInProgress = false;
     if (status !== 'success') {
         console.log('Unable to access network');
         phantom.exit();
     } else {
-        console.log("load finished");
+        console.log('load finished');
     }
 };
 
-var steps = [
-    function() {
+let steps = [
+    function () {
         page.open('https://tomusss.univ-lyon1.fr/');
     },
-    function() {
-        page.injectJs("https://code.jquery.com/jquery-2.2.4.min.js");
-        page.evaluate(function() {
+    function () {
+        page.injectJs('https://code.jquery.com/jquery-2.2.4.min.js');
+        page.evaluate(function () {
             document.getElementById('username').value = 'p0907931';
             document.getElementById('password').value = 'XXXXXXXX';
             $('.btn-submit').click();
         });
     },
-    function() {
+    function () {
         console.log('Answers:');
         page.evaluate(function () {
-            var arrayUes = document.getElementsByClassName('DisplayUE');
-            for(var i = 0; i < arrayUes.length; i++){
-                var arrayUe = arrayUes[i];
+            let arrayUes = document.getElementsByClassName('DisplayUE');
+            for (let i = 0; i < arrayUes.length; i++) {
+                let arrayUe = arrayUes[i];
                 console.log(arrayUe.getElementsByClassName('DisplayUETitle')[0].innerText);
-                var arrayCellBox = arrayUe.getElementsByClassName('DisplayCellBox');
-                for(var j = 0; j < arrayCellBox.length; j++){
-                    var title = arrayCellBox[j].getElementsByClassName('DisplayCellTitle')[0].innerText;
-                    var value = arrayCellBox[j].getElementsByClassName('DisplayCellValue')[0].innerText;
+                let arrayCellBox = arrayUe.getElementsByClassName('DisplayCellBox');
+                for (let j = 0; j < arrayCellBox.length; j++) {
+                    let title = arrayCellBox[j].getElementsByClassName('DisplayCellTitle')[0].innerText;
+                    let value = arrayCellBox[j].getElementsByClassName('DisplayCellValue')[0].innerText;
                     value = value.trim();
-                    if(value && value != ""){
-                        console.log('Examen: '+ title+ ' => '+ value);
+                    if (value && value != "") {
+                        console.log('Examen: ' + title + ' => ' + value);
                     }
                 }
             }
         });
     },
-    function() {
+    function () {
         console.log('Exiting');
     }
 ];
 
-var interval = setInterval(function() {
-    if (!loadInProgress && typeof steps[testindex] == "function") {
-        console.log("step " + (testindex + 1));
+setInterval(function () {
+    if (!loadInProgress && typeof steps[testindex] === 'function') {
+        console.log('step ' + (testindex + 1));
         steps[testindex]();
-        testindex++;
+        testindex = testindex + 1;
     }
-    if (typeof steps[testindex] != "function") {
-        console.log("test complete!");
+    if (typeof steps[testindex] !== 'function') {
+        console.log('test complete!');
         phantom.exit();
     }
 }, 50);
