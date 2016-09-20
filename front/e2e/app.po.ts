0acfb03258f820211fc51daf1071542379730486
Antoine Martin <hyptos974@gmail.com>
2016-09-20 15:24:24 +0200

    ajout du projet front sous angular 2

---
+++ front/e2e/app.po.ts
@@ -0,0 +1,11 @@
+import { browser, element, by } from 'protractor/globals';
+
+export class FrontPage {
+  navigateTo() {
+    return browser.get('/');
+  }
+
+  getParagraphText() {
+    return element(by.css('app-root h1')).getText();
+  }
+}
